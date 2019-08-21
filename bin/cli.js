8c7b4f3918ca2edf3881ed8322be1da28db47a60
mingshined <996578843@qq.com>
2019-08-21 10:16:10 +0800

    update

--- bin/cli.js
+++ bin/cli.js
@@ -1,50 +1,32 @@
 #!/usr/bin/env node --harmony
-'use strict'
+"use strict";
 
-process.env.NODE_PATH = __dirname + '/../node_modules/'
+process.env.NODE_PATH = __dirname + "/../node_modules/";
 
-const program = require('commander')
+const program = require("commander");
 
-program
-	.version(require('../package').version )
+program.version(require("../package").version);
 
-program
-	.usage('<command>')
+program.usage("<command>");
 
 program
-	.command('add')
-	.description('添加一个模板')
-  .alias('a')
+  .command("list")
+  .description("显示所有模板")
+  .alias("l")
   .action(() => {
-    require('../src/command/add')()
-  })
+    require("../src/command/list")();
+  });
 
 program
-	.command('list')
-	.description('显示所有模板')
-	.alias('l')
-	.action(() => {
-		require('../src/command/list')()
-	})
-
-program
-	.command('init')
-	.description('生成项目')
-  .alias('i')
+  .command("init")
+  .description("生成项目")
+  .alias("i")
   .action(() => {
-    require('../src/command/init')()
-  })
+    require("../src/command/init")();
+  });
 
-program
-	.command('delete')
-	.description('删除模板')
-	.alias('d')
-	.action(() => {
-		require('../src/command/delete')()
-	})
+program.parse(process.argv);
 
-program.parse(process.argv)
-
-if(!program.args.length){
-  program.help()
+if (!program.args.length) {
+  program.help();
 }
